ece26ac70eb72a20b0546a99d3390e305da30e8f
Waarith <2006abdulwaris@gmail.com>
2025-07-05 18:29:18 +0100

    Counter js has been added

--- counter/counter.js
+++ counter/counter.js
@@ -1,2 +1,20 @@
-const names = "rok "
-console.log(names);
+const increaseBtn = document.getElementById("increaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const decreaseBtn = document.getElementById("decreaseBtn")
+const countlabel = document.getElementById("counter")
+let count = 0;
+
+const firstcounter = () => {
+    count ++;
+    countlabel.textContent = count 
+}
+
+const secondcounter = () => {
+    count = 0;
+    countlabel.textContent = count 
+}
+
+const thirdcounter = () => {
+    count --;
+    countlabel.textContent = count 
+}
